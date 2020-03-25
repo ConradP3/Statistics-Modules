049d9ffaca7205fc5fa6511aa8860a261e5e9372
colperei <colperei@ucsc.edu>
2020-03-24 23:27:08 -0700

    2SampleTtest module + Update

--- 2SampleTTestModule.py
+++ 2SampleTTestModule.py
@@ -4,14 +4,15 @@
 
 ##Conrad Pereira
 
-##T Test Module
+##2 Sample T Test Module
 
-##Performs one sample mean T Test
+##Performs 2 sample mean T Test
 ##Given:
 ##      Mean
+##      2nd Mean
 ##      Null Hypothesis
-##      Standard Deviation
-##      Sample Size
+##      Standard Deviations
+##      Sample Sizes
 ##  --> Computes T-Stat
 ##      Confidence Level
 ##      Compares T-critcal value with T-stat
@@ -29,9 +30,8 @@ import numpy as np
 from scipy import stats
 import math
 
-
 def clicked():
-    messagebox.showinfo('One Sample TTest Module Description', 'Software module to  automatically perform one sample mean T Test given the sample mean, null hypothesis, standard deviation, and confidence interval. Assumptions: Sample size is sufficiently large enough to perform Test')
+    messagebox.showinfo('Two Sample TTest Module Description', 'Software module to  automatically perform two sample mean T Test given the sample means, null hypothesis, standard deviations, and confidence interval. Assumptions: Sample sizes are sufficiently large enough to perform Test')
 
 def confidenceLevel(TCritCon):
     if(TCritCon == 90):
@@ -43,13 +43,17 @@ def confidenceLevel(TCritCon):
 
 def clickedTest():
     Samplemean = float(txtSamplemean.get())
+    Samplemean2 = float(txtSamplemean2.get())
     Nullhyp = float(txtNull.get())
-    StandardDev = float(txtSd.get())
+    Variance = float(txtVar.get())
+    Variance2 = float(txtVar2.get())
     Samplesize=float(txtSamplesize.get())
+    Samplesize2=float(txtSamplesize2.get())
+
     TCrit=float(txtConfidencelvl.get())
     #TCrit = float(comboConfidencelvl.get())
 
-    TStat = (Samplemean-Nullhyp)/(StandardDev/(math.sqrt(Samplesize)))
+    TStat = (Samplemean-Samplemean2-Nullhyp)/(math.sqrt((Variance/Samplesize)+(Variance2/Samplesize2)))
 
     if(TCrit == 90):
         TCrit = 1.65
@@ -73,7 +77,7 @@ window = tk.Tk()
 
 logo = tk.PhotoImage(file="Ttest.png")
 
-window.title("Single Sample T-Test") 
+window.title("Two Sample T-Test")
 window.geometry('1000x1000')
 
 w1 = tk.Label(window, image=logo)
@@ -85,6 +89,12 @@ lblSamplemean.grid(column=0, row=6)
 txtSamplemean = Entry(window,width=20)
 txtSamplemean.grid(column=1, row = 6)
 
+lblSamplemean2 = Label(window, text ="2nd Sample Mean")
+lblSamplemean2.grid(column=0, row=7)
+
+txtSamplemean2 = Entry(window,width=20)
+txtSamplemean2.grid(column=1, row = 7)
+
 lblNullhyp = Label(window, text ="Null Hypothesis")
 lblNullhyp.grid(column=0, row=8)
 
@@ -92,11 +102,17 @@ txtNull = Entry(window,width=20)
 txtNull.grid(column=1, row = 8)
 
 
-lblStandarddev = Label(window, text ="Standard Deviation")
-lblStandarddev.grid(column=0, row=10)
+lblVariance = Label(window, text ="Variance")
+lblVariance.grid(column=0, row=10)
 
-txtSd = Entry(window,width=20)
-txtSd.grid(column=1, row = 10)
+txtVar = Entry(window,width=20)
+txtVar.grid(column=1, row = 10)
+
+lblVariance2 = Label(window, text ="2nd Sample's Variance")
+lblVariance2.grid(column=0, row=11)
+
+txtVar2 = Entry(window,width=20)
+txtVar2.grid(column=1, row = 11)
 
 txtSamplesize = Entry(window,width=20)
 txtSamplesize.grid(column=1, row = 12)
@@ -104,14 +120,20 @@ txtSamplesize.grid(column=1, row = 12)
 lblSamplesize = Label(window, text ="Sample Size")
 lblSamplesize.grid(column=0, row=12)
 
+txtSamplesize2 = Entry(window,width=20)
+txtSamplesize2.grid(column=1, row = 14)
+
+lblSamplesize2 = Label(window, text ="2nd Sample Size")
+lblSamplesize2.grid(column=0, row=14)
+
 ##comboConfidencelvl = Combobox(window)
 ##comboConfidencelvl['values']= (1.65,1.95,2.58)
 ##comboConfidencelvl.grid(column=1, row = 12)
 txtConfidencelvl = Entry(window,width=20)
-txtConfidencelvl.grid(column=1, row = 14)
+txtConfidencelvl.grid(column=1, row = 16)
 
 lblConfidencelvl = Label(window, text ="Confidence Level (Enter 90, 95 or 99)")
-lblConfidencelvl.grid(column=0, row=14)
+lblConfidencelvl.grid(column=0, row=16)
 
 ##explanation = """Software module to perform a T Test"""
 ##lbl = Label(window, text=explanation, )
@@ -121,9 +143,9 @@ lblans = Label(window, text = "")
 lblans.grid(column=1, row=20)
 
 
-btnTtest = Button(window, text="Single Sample T Test", command=clickedTest)
+btnTtest = Button(window, text="Two Sample T Test", command=clickedTest)
 
-btnTtest.grid(column=1, row=16)
+btnTtest.grid(column=1, row=18)
 
 btn = Button(window,text='TTest Module Explanation', command=clicked)
 
--- ModuleInterface.py
+++ ModuleInterface.py
@@ -24,28 +24,79 @@ import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
+import tkinter.font as tkFont
 from subprocess import call
 
+
 class callModules(object):
-    
-    def __init__(self, path ='/Users/conradpereira/Desktop/Statistics-Modules/TTestModule.py'):
+
+    def __init__(self, path ='/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/TTestModule.py'):
         self.path = path
-        
+
     def callTTestModule(self):
+        #path ='/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/TTestModule.py'
+        call(["Python3", "{}".format(self.path)])
+
+class callModules2(object):
+    def __init__(self, path = '/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/2SampleTTestModule.py'):
+        self.path = path
+    def callTTestModule2(self):
+        call(["Python3", "{}".format(self.path)])
+
+class callModules3(object):
+    def __init__(self, path = '/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/RegCoeffModule.py'):
+        self.path = path
+    def callRegcoeff(self):
+        call(["Python3", "{}".format(self.path)])
+
+class callModules4(object):
+    def __init__(self, path = '/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/ANOVAModule.py'):
+        self.path = path
+    def callAnova(self):
+        call(["Python3", "{}".format(self.path)])
+
+class callModules5(object):
+    def __init__(self, path = '/Users/conradpereira/Desktop/UCSC/Projects/Statistics-Modules/MeasuresModule.py'):
+        self.path = path
+    def callMeasures(self):
         call(["Python3", "{}".format(self.path)])
 
 
-
-
-    
 def clickedTtest():
     callMod = callModules()
     callMod.callTTestModule()
-def clicked():
-    messagebox.showinfo('Click one ofSeveral software modules that automatically performs different statistical tests. (One Sample T Test, Two independent samples t-test, One-way ANOVA, Regression Coefficient Testing, etc.')
+
+def clickedTtest2():
+    callMod = callModules2()
+    callMod.callTTestModule2()
+
+def clickedRegcoeff():
+    callMod = callModules3()
+    callMod.callRegcoeff()
+
+def clickedAnova():
+    callMod = callModules4()
+    callMod.callAnova()
+
+def clickedMeasures():
+    callMod = callModules5()
+    callMod.callMeasures()
+
+
+def clicked(): #HELP
+    messagebox.showinfo('Help','Click one ofSeveral software modules that automatically performs different statistical tests. (One Sample T Test, Two independent samples t-test, One-way ANOVA, Regression Coefficient Testing, etc.')
+
+
+
 window = tk.Tk()
-window.title("Statistics Modules") 
+
+window.title("Statistics Modules")
 window.geometry('1000x1000')
+stats = tk.PhotoImage(file="Stats.png")
+w1 = tk.Label(window, image=stats)
+w1.grid(column=4, row =0)
+
+
 
 btn = Button(window,text='Help', command=clicked)
 btn.grid(column=0,row=30)
@@ -53,16 +104,19 @@ btn.grid(column=0,row=30)
 lblTitle = Label(window, text= "Statistical Modules")
 lblTitle.grid(column=1, row = 0)
 
+btnMeasures= Button(window, text="Measures of Central Tendancy", command=clickedMeasures)
+btnMeasures.grid(column=1, row=2)
+
 btnTtest = Button(window, text="Single Sample T Test", command=clickedTtest)
 btnTtest.grid(column=1, row=4)
 
-btnTtest = Button(window, text="Two Sample T Test", command=clickedTtest)
-btnTtest.grid(column=1, row=6)
+btnTtest2 = Button(window, text="Two Sample T Test", command=clickedTtest2)
+btnTtest2.grid(column=1, row=6)
 
-btnTtest = Button(window, text="Regression Coefficeint T Test", command=clickedTtest)
-btnTtest.grid(column=1, row=8)
+btnRegcoeff = Button(window, text="Regression Coefficient T Test", command=clickedRegcoeff)
+btnRegcoeff.grid(column=1, row=8)
 
-btnTtest = Button(window, text="ANOVA", command=clickedTtest)
-btnTtest.grid(column=1, row=10)
+btnAnova = Button(window, text="ANOVA", command=clickedAnova)
+btnAnova.grid(column=1, row=10)
 
 window.mainloop()
